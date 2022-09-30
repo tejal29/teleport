09dd4abee4213a95358d75c47af938627d78fc9b
Edward Dowling <EdwardDowling@users.noreply.github.com>
2022-09-30 13:36:53 +0100

    Update api/types/constants.go
    Co-authored-by: Jeff Pihach <hatched@users.noreply.github.com>

--- api/types/constants.go
+++ api/types/constants.go
@@ -460,7 +460,7 @@ const (
 	AlertLink = "teleport.internal/link"
 
 	// AlertLicenseExpired is an internal label that indicates that the license has expired.
-	AlertLicenseExpired = "teleport.internal/license-warning-expired"
+	AlertLicenseExpired = "teleport.internal/license-expired-warning"
 )
 
 // RequestableResourceKinds lists all Teleport resource kinds users can request access to.
